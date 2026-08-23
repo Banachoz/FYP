8c1b92661df160a799504d34c496d02d312176de
Banachoz <Banachos12332@gmail.com>
2026-08-23 19:40:27 +0800

    Add rep completion sound cue

---
+++ core/rep_sound.py
@@ -0,0 +1,28 @@
+import os
+import threading
+
+_SOUND_PATH = os.path.join(os.path.dirname(__file__), "..", "audio_cue", "osuhit.wav")
+
+
+class RepSound:
+    def __init__(self):
+        self._mixer = None
+        self._sound  = None
+        self._ready  = False
+        threading.Thread(target=self._load, daemon=True).start()
+
+    def _load(self):
+        try:
+            import pygame
+            pygame.mixer.init()
+            self._sound = pygame.mixer.Sound(_SOUND_PATH)
+            self._ready = True
+        except Exception:
+            pass
+
+    def play(self):
+        if self._ready:
+            try:
+                self._sound.play()
+            except Exception:
+                pass
